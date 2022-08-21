5cf3d7fd37c56e273918d0b366ae1cc750058fca
Alain Kimbu <54691684+Rodern@users.noreply.github.com>
2022-08-21 06:50:05 +0100

    mades changes on what files to cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -14,11 +14,26 @@ const urls = [
     '/styles/index.css',
     '/scripts/index.js',
     '/manifest.json',
+    'favicon.ico',
+    'apple-touch-icon.png',
     '/serviceWorker.js',
-    '/assets/vendors/jquery-3.6.0.min.js'
+    '/assets/vendors/jquery-3.6.0.min.js',
+    '/assets/vendors/tailwind/font.awesome.css.js',
+    '/assets/vendors/tailwind/tailwindcss.elements.min.css',
+    '/assets/vendors/tailwind/tailwindcss.elements.min.js',
+    '/assets/vendors/tailwind/tailwindcss.css',
+    '/assets/vendors/tailwind/tailwindcss.js',
+    '/assets/vendors/workbox/workbox-v6.5.4/workbox-sw.js',
+    '/assets/vendors/workbox/workbox-strategies.prod.js',
+    '/assets/vendors/workbox/workbox-core.prod.js',
+    '/assets/vendors/workbox/workbox-recipes.prod.js',
+    '/assets/vendors/workbox/workbox-routing.prod.js',
+    '/assets/vendors/workbox/workbox-cacheable-response.prod.js',
+    '/assets/vendors/workbox/workbox-expiration.prod.js',
+    '/assets/vendors/workbox/precaching.prod.js'
 ];
 
-workbox.recipes.warmStrategyCache({urls, strategy})
+workbox.recipes.warmStrategyCache({ urls, strategy })
 
 workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
@@ -26,7 +41,7 @@ workbox.routing.registerRoute(
 )
 
 self.addEventListener('fetch', event => {
-    if (event.request.url.endsWith('.png')) {
+    if (event.request.url.endsWith('.png') || event.request.url.endsWith('.json') || event.request.url.endsWith('.ico') || event.request.url.endsWith('.jpg') || event.request.url.endsWith('.js') || event.request.url.endsWith('.css')) {
         // Oops! This causes workbox-strategies.js to be imported inside a fetch handler,
         // outside of the initial, synchronous service worker execution.
         const cacheFirst = new workbox.strategies.CacheFirst();
