bf7f819f3c376b99c35f90c8f3197124b84ef282
Alain Kimbu <54691684+Rodern@users.noreply.github.com>
2022-11-14 10:34:30 +0100

    chore: social links and page header

--- scripts/index.js
+++ scripts/index.js
@@ -450,7 +450,7 @@ window.onpageshow = function name(params) {
 
 
 const list1 = ".ms-btn, .ns-btn, .bs-btn, .ml-btn, .nl-btn, .bl-btn, .for-o, .p-avt"
-const list2 = ".m-usr, .n-usr, .for-l"
+const list2 = ".m-usr, .n-usr, .for-l, .mlg-btn"
 function chAuth(){
     if (_Detail.userId === undefined || _user.userId === undefined) {
         //console.log(true)
@@ -459,7 +459,14 @@ function chAuth(){
     }
     else {
         //console.log(false)
-        $(' .m-usr > span, .n-usr > span').text(_user.firstName + " " + _user.lastName);
+        //$(' .m-usr > span, .n-usr > span').text(_user.firstName + " " + _user.lastName);
+        //$(' .m-usr > span, .n-usr').append(``)
+        $('.user-svg').remove()
+        $('.nav-links.n-usr').append(`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    class="w-5 h-5 ml-1 user-svg">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>`)
         if (_user.image.length != 0)
             $('.u-img').attr('src', 'data:image/png;base64,'+_user.image)
         $("" + list1 + "").hide()
